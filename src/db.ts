b7bed24d0ac3b2d4e9f55ee18521198994952a73
Jean Evenx <jeanlovenx@gmail.com>
2022-04-06 20:51:45 -0300

    Remove the connectionString

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString  = 'postgres://gmzxbmef:pR6SKqK1xhr9uEB6QQtNJ0MStbDmd8dH@motty.db.elephantsql.com/gmzxbmef';
+const connectionString  = '';
 
 const db = new Pool({
     connectionString,
